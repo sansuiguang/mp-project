1e6ffd65f4025996f4d574ebb1a4e6c45b456131
sansuiguang <326121753@qq.com>
2019-05-21 10:37:04 +0800

    完成轮播图部分

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <search-bar/>
+    <!-- 轮播图部分 -->
+    <swiper :indicator-dots="indicatorDots">
+      <swiper-item :key="index" v-for="(item,index) in imgUrls">
+        <image :src="item" class="slide-image"/>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
@@ -9,7 +15,15 @@
 import SearchBar from "../../components/search";
 export default {
   data() {
-    return {};
+    return {
+      //该属性的作用显示轮播图小圆点
+      indicatorDots: true,
+      imgUrls: [
+        "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",
+        "https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640",
+        "https://images.unsplash.com/photo-1551446591-142875a901a1?w=640"
+      ]
+    };
   },
   components: {
     "search-bar": "SearchBar"
@@ -18,4 +32,7 @@ export default {
 </script>
 
 <style  scoped>
+.slide-image {
+  width: 750rpx;
+}
 </style>
